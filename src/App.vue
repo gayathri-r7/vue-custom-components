64d750b0873a41fd6d00fecf1e54326ef9804a1c
Gayathri R <gayathri.r@gopaddle.io>
2023-09-17 11:07:49 +0530

    recent changes

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,65 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TableComponent :rows="rows" :columns="columns" resourceType="employee">
+    <template v-slot:tb-column="props">
+      <span v-if="props.column.label === 'Employee ID'">
+          <span>
+            {{props.column.label}} <i class="fa fa-bell-o" style="color:blue"></i> 
+          </span>
+      </span>
+      <span v-else>
+        {{props.column.label}}
+      </span>
+    </template>
+    <template v-slot:tb-row="props">
+      <span v-if="props.column.key === 'id'">
+        <code>EMP - {{props.row.id}}</code>
+      </span>
+    </template>
+  </TableComponent>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TableComponent from './components/Tables/Table.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TableComponent
+  },
+  data () {
+    return {
+      rows: [
+        {
+          id: 100,
+          name: 'gayathri',
+          job: 'VueJs Developer'
+        },
+        {
+          id: 101,
+          name: 'Priya',
+          job: 'React Developer'
+        },
+        {
+          id: 102,
+          name: 'Anu',
+          job: 'Angular Developer'
+        }
+      ],
+      columns: [
+        {
+          key: 'id',
+          label: "Employee ID"
+        },
+        {
+          key: 'name',
+          label: "Employee Name"
+        },
+        {
+          key: 'job',
+          label: "Job Title"
+        }
+      ]
+    }
   }
 }
 </script>
---
+++ src/components/Tables/Table.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th v-for="(column, labelIndex) in columns" :key="labelIndex">
+            <slot name="tb-column" :columnIndex="labelIndex" :column="column">
+              {{ column.label }}
+            </slot> 
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, itemIndex) in rows" :key="itemIndex">
+            <td v-for="(column, labelIndex) in columns" :key="labelIndex">
+                <slot name="tb-row" :row="item" :rowIndex="itemIndex" :column="column">
+                    {{ item[column.key] }}
+                </slot> 
+            </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'TableComponent',
+  components: {},
+  props: ['rows', 'columns', 'resourceType'],
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+table {
+  border-collapse: collapse;
+  width: 100%;
+}
+tr:hover {
+    background-color: ghostwhite;
+    cursor: pointer;
+}
+table tr {
+    border-bottom: 1px solid #f2f2f2;
+}
+tr:first-child {
+  border-top: none;
+}
+tr:last-child {
+  border-bottom: none;
+}
+th, td {
+  text-align: left;
+  padding: 8px;
+}
+th {
+  background-color: #f2f2f2;
+  color: black;
+  text-transform: uppercase;
+  /* font-weight: normal; */
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 80%;
+}
+</style>
